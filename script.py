50d44ebf3b62d5c74afaabfeb59787d8f3bda0d8
comddy <2182433986@qq.com>
2025-08-17 15:56:27 +0800

    用ssl

--- script.py
+++ script.py
@@ -1071,5 +1071,4 @@ def learning_percent():
     })
 
 if __name__ == '__main__':
-    # app.run(debug=True, host='0.0.0.0', port=5000, ssl_context=('deepspring-tech.com.pem', 'deepspring-tech.com.key'))
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000, ssl_context=('deepspring-tech.com.pem', 'deepspring-tech.com.key'))
