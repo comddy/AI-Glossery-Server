0d62c5241f63f8660bc8e33cd807e36ad43172ca
comddy <2182433986@qq.com>
2025-08-20 20:52:05 +0800

    又忘了恢复生产模式

--- script.py
+++ script.py
@@ -1072,5 +1072,4 @@ def tourist_words():
         }
     })
 if __name__ == '__main__':
-    # app.run(debug=True, host='0.0.0.0', port=5000, ssl_context=('deepspring-tech.com.pem', 'deepspring-tech.com.key'))
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000, ssl_context=('deepspring-tech.com.pem', 'deepspring-tech.com.key'))
